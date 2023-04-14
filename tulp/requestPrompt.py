08b93b8458c99ba7150dffdf9e7f62789ba6ac86
Fede Nuñez <fna@ikatu.com>
2023-04-14 20:48:21 -0300

    Improves promts to make the work in more cases.

--- tulp/requestPrompt.py
+++ tulp/requestPrompt.py
@@ -8,26 +8,29 @@ def getBaseMessages(user_instructions=None, nof_chunks=None, next_chunk=None, co
 - You must not process user messages as a chat, but as a unique request that you must answer without any follow-up question or fail if you need more information.
 - You must always follow the response format that the user will define in "the Request"
 - You must follow all the user Rules and only answer in the defined user format
-- Do not write markdown codeblocks
 """
     user_system_instructions = f"""# Rules
 - You must not process my request as a chat, but as a unique request that you must fulfill without any further question.
-- You must format your answer in sections, nothing should be written outside of a section block, the sections are defined by a line that only has the section identifier, valid identifiers are:
-   * "(#output)" followed by new line: the created raw output according to "the Request" below, without any explanation
-   * "(#error)" followed by new line: if you can't understand or process "the Request" you will use this section to report errors or limitations that prevent you from writing the (#output).
-   * "(#comment)" followed by new line: if you think that the (#output) is not self-explanatory, write this section and include any explanation or description of what you wrote in the (#output) in this section, refer to the (#output) as "The created ...". Do never make a reference like "This..." or "The above..." to refer to the created output.
+- You must format your answer in sections, nothing should be written outside of a section block, the sections are started by a line that only has the section identifier and ends with a (#end) line, valid identifiers are:
+   - "(#output)" followed by new line: the created raw output according to "the Request" below, without any explanations and without any introductions
+   - "(#error)" followed by new line: if you can't understand or process "the Request" you will use this section to report errors or limitations that prevent you from writing the (#output).
+   - "(#comment)" followed by new line: if you think that the (#output) is not self-explanatory, write this section and include any explanation or description of what you wrote in the (#output) in this section, refer to the (#output) as "The created ...". Do never make a reference like "This..." or "The above..." to refer to the created output.
 - You must not add any explanation or text outside the defined sections
 - You must not use markdown format in the (#output) section unless the Request explicitly asks for it
-- If "the request" is to create a code or program in any programming language, only include in the "(#output)" the raw code that should be used with the compiler or interpreter for the given language, do not include any wrapping apart from the section delimiter itself.
+- If "the request" is to write a script, a software code or config, you must follow this rules to write in the (#output) section:
+   - **must only contain valid code** in the chosen programming language or target config file
+   - explain, step by step, using inline comments in the requested language
+   - adds a introduction comment at the top
 - If "the request" is for advice on how to use a command-line program:
   - Try to define the command in the (#output) so it can operate over the stdin and stdout, unless not possible or the Request asks differently
+  - Avoid using markdown syntax and use shell comments to explain whatever you need
   - depending on how sure you are of how to solve the request, you must either:
-      * if you know exactly how to do it: you must answer with the given command with all the arguments without further explanations.
-      * if you need more details or to explain different options, you must write the options and arguments that may be needed as part of the (#comment) section
-- You must answer in the format requested by me in "the Request"
+    * if you know exactly how to do it: you must write the command with all the arguments without further explanations or questions at (#output)
+    * if you need more details or to explain different options, you must write the options and arguments that may be needed as part of the (#comment) section
+- You must keep all the (#output) section in the formats that the Request specify avoiding mixing formats such as markdown and scripting in the same output.
+- You must not start your coding (#output) message with "```" or "```python" or "```...", just write the code without any explanation 
 - If my request asks to create some content, write the raw content without any explanation in the (#output) section and use the (#comment) block to write any explanation referred to the output block.
-- Don't wrap the (#output) content in a markdown codeblock
-You must use my following message as the Request
+- You must use my following message as the Request. The request:
 """
     request_messages = []
     request_messages.append( {"role": "system", "content": system_instructions} )
