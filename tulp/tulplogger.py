b5d4024d529f08944f515316b2840a3e31b9b2ed
Fede Nuñez <fna@ikatu.com>
2023-04-22 12:51:33 -0300

    v0.5 development Changes output color

--- tulp/tulplogger.py
+++ tulp/tulplogger.py
@@ -10,6 +10,17 @@ RED    = '\033[31m'
 BOLD   = '\033[1m'
 RESET  = '\033[0m'
 GREY   = '\033[90m'
+#GREY_0 = "\033[38;5;232m"
+GREY_10 = "\033[38;5;242m"
+GREY_11 = "\033[38;5;243m"
+GREY_13 = "\033[38;5;245m"
+GREY_14 = "\033[38;5;246m"
+GREY_15 = "\033[38;5;247m"
+GREY_16 = "\033[38;5;248m"
+GREY_17 = "\033[38;5;249m"
+GREY_18 = "\033[38;5;250m"
+
+
 
 
 useColors = False
@@ -26,45 +37,25 @@ def print_color(text, color):
     else:
         print(text,file=sys.stderr)
 
-def print_yellow(text):
-    """Prints text in yellow"""
-    print_color(text, YELLOW)
-
-def print_orange(text):
-    """Prints text in orange"""
-    print_color(text, ORANGE)
-
-def print_red(text):
-    """Prints text in red"""
-    print_color(text, RED)
-
-def print_bold(text):
-    """Prints text in bold"""
-    print_color(text, BOLD)
-
-def print_grey(text):
-    """Prints text in bold"""
-    print_color(text, GREY)
-
 class Logger:
     def __init__(self):
         self.log_level = config.log_level
 
     def error(self, message):
         if self.log_level in ['ERROR', 'WARNING', 'INFO', 'DEBUG']:
-            print_red(f'[ERROR] {message}')
+            print_color(f'[ERROR] {message}',RED)
 
     def warning(self, message):
         if self.log_level in ['WARNING', 'INFO', 'DEBUG']:
-            print_orange(f'[WARNING] {message}')
+            print_color(f'[WARNING] {message}',ORANGE)
 
     def info(self, message):
         if self.log_level in ['INFO', 'DEBUG']:
-            print_bold(f'[INFO] {message}')
+            print_color(f'[INFO] {message}',GREY_18)
 
     def debug(self, message):
         if self.log_level == 'DEBUG':
-            print_grey(f'[DEBUG] {message}')
+            print_color(f'[DEBUG] {message}',GREY_11)
 
 if __name__ == '__main__':
     # Example usage
