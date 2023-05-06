26e8137db66dc0367bc94aa0e1f2a2451f62350c
Fede Nuñez <fna@ikatu.com>
2023-05-06 13:18:52 -0300

    I am a nicer CLI demo recorder.

--- examples/demo/play_and_rec_demo.sh
+++ examples/demo/play_and_rec_demo.sh
@@ -1,6 +1,18 @@
-
 #!/bin/bash
 
+
+# check arguments
+if [ -z "$1" ]; then
+  echo "Error: Argument is not defined. Please provide a valid demo command file (e.g: $0 demo_commands.sh)."
+  exit 1
+fi
+
+if [ ! -f "$1" ]; then
+  echo "Error: Argument does not point to a valid file. Please provide a valid demo commands file (e.g: $0 demo_commands.sh)."
+  exit 1
+fi
+
+# killall background process
 function cleanup {
     echo "Cleaning up..."
     sleep 1
@@ -11,18 +23,25 @@ function cleanup {
 
 trap cleanup INT
 
-Xephyr -screen 1200x600 :1 &
+
+# Start nested X
+Xephyr -screen 1600x1200 :1 &
 export DISPLAY=:1
-sleep 1
-blackbox &
-sleep 1
-feh --bg-scale ~/Desktop/Desktop_23_04_2023/AI.jpeg
 
-filename="video_$(date +%Y%m%d_%H%M%S).mp4"
+# start the windows manager
+sleep 1
+mutter  --sm-disable --sync  -&
+sleep 0.5
+
+# start the recording process
+filename="video_$(date +%Y%m%d_%H%M%S).webm"
 echo "$filename"
-(sleep 0.5; ffmpeg -f x11grab -video_size $(xdpyinfo | grep dimensions | awk '{print $2;}') -i :1.0  $filename )&
+(sleep 1.5; ffmpeg -f x11grab  -video_size $(xdpyinfo | grep dimensions | awk '{print $2;}') -i :1.0 -c:v libvpx-vp9 -lossless 1 $filename )&
 
-terminology -f mono/24 -2 -M -e bash ./play_demo.sh
-
+# center the window
+(sleep 1
+wmctrl -r :ACTIVE: -e 0,150,150,1300,900
+)&
+terminology -f mono/24 -M --geometry +0-0 -T  tulp -e "sleep 1; bash ./play_demo.sh $1"
 cleanup
 
