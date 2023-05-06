3ba77cfcd1cc96e7dd8743564c20d0ed7f89bcb7
Fede Nuñez <fna@ikatu.com>
2023-05-06 16:39:02 -0300

    Updates demo commands.

--- examples/demo/demo_commands.sh
+++ examples/demo/demo_commands.sh
@@ -1,15 +1,15 @@
 
 # Define the list of commands that will be shown by the demo
 commands=(
-'tulp find all the files in my home folder that are bigger than 1 megabyte and where changed in the last month'
-'tulp write a python program that say \"Welcome to tulp AI driven CLI\", using a gradient colors \(from red to green\) using ansi colors, one color per letter | python'
-"echo \"Natural language to plot:\""
-'tulp write a python program that plots 1,1 2,2 3,3 with the leyend tulp line using mathplotlib | python'
-"echo \"Now let's see how data extraction works:\""
-'cat ../oranges_poem.txt'
-'cat ../oranges_poem.txt | tulp write a json file that list all the persons and the oranges that they do have'
-"echo \"Now let's see how to pipe tulp to tulp\""
-'tulp write a story about Alice, Bob and Joe, they will find 10 oranges, 10 apples and 10 bananas, then they will share them | tulp write a python program creates a stacking graph with the fruits that each has using mathplotlib | python'
-'head -n 6 ../../README.md | tulp translate to Chinese'
-'vim ../oranges_poem.txt'
+'tulp Write a python program that writes "Welcome to tulp AI driven CLI" using an 254 color gradient from red to green | python '
+'tulp find all the license files in the folder /usr/share that are older than 1 year and contain the string  GPL | bash'
+'echo "Julia has one dog and Cecilia has one cat" | tulp create a csv file with the columns name and pets '
+'echo "Julia has one dog and Cecilia has one cat" | tulp create a json file with a dictionary of persons and pets '
+'echo "Julia has one dog and Cecilia has one cat" | tulp  cecilia has one cat, add also a dog'
+'echo "Julia has one dog and Cecilia has one cat and also one dog" | tulp change all the occurrences of dog to parrot'
+'echo "Julia has one parrot and Cecilia has one cat and also one parrot" | tulp make a python program that plots the number of pets each has using mathplotlib, stacking by type | python'
+'tulp write a python program that create an animated plot of a sine wave with a dot that goes over it using matplotlib.animation, the leyend should be tulp animated sine | python'
+'tulp write a paragraph of a story about Alice, Bob finding 10 oranges and sharing them | tee /tmp/story.txt'
+'cat /tmp/story.txt | tulp translate to Chinese '
+'vim /tmp/story.txt'
 )
