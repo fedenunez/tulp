32e6b3e4ba43111b4ca555147b5b0bf3291156a0
Fede Nuñez <fna@ikatu.com>
2023-05-18 20:01:27 -0300

    Improves createFilteringPrompt

--- tulp/createFilteringProgramPrompt.py
+++ tulp/createFilteringProgramPrompt.py
@@ -8,20 +8,21 @@ def getMessages(user_instructions, raw_input, nof_chunks=None, next_chunk=None, 
     request_messages = []
 
     user_system_instructions = f"""# Rules
-- Your response should be split into blocks, valid blocks are: (#inner_messages), (#output), (#comment); the (#output) is mandatory.
-- Your task is to write a python program (into the (#output) block), 
+- Your response should be split into blocks, valid blocks are: (#inner_messages), (#thoughts), (#output), (#comment); the (#output) is mandatory.
+- Your task is to write a python program
 - Writing the code in the (#output) block:
-  - Start the program with an inline comment with the overall description of the software design
+  - Start the program with an inline comment with the description of the code that you will write.
   - then write all the needed import, verify that all are included!
-  - if the request is to process or filter the input, now add the code that will load the input into the buffer.
-  - continue writing the program step by step, adding inline comments for each step to make it more understandable
+  - if and input file is needed: write the code that reads the stdin into a input buffer to be processed
+  - continue writing the program step by step
+  - add inline comments before each important code section explaining the functionality and rational behind it
   - verify at every step that you made the needed import before using any module
 
 # Response template:
 {""}(#output)
 <write the output program in python. This block is mandatory>
 {""}(#comment)
-<An overall description of what you wrote on (#output) and how you created. Any extra explanation, comment, or reflection you may have regarding the generated (#output). Do not ever make a reference like "This..." or "The above..." to refer to the created output. Remember to mention any external module that the user should install using pip install ... >
+<An overall description of what you wrote on (#output) and how you created. Remember to mention any external module that the user should install using pip install ... >
 
 # Request:
 You must create a python program that fulfil:
