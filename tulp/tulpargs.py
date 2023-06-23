84826cc9cdbd2668d4b2d3e38ac4528fdac17518
Fede Nuñez <fna@ikatu.com>
2023-06-23 08:14:21 -0300

    v0.8
    Changes default model to gpt-3.5-turbo-16k
    Raises default max chars to 12k
    Accept any model name starting with gpt- as argument

--- tulp/tulpargs.py
+++ tulp/tulpargs.py
@@ -1,10 +1,17 @@
 import os
 import argparse
+import re
 
 CONFIG_FILE=os.path.expanduser("~/.tulp.conf")
 
+def model_type(arg_value, pat=re.compile(r"^gpt-[a-z0-9\-_\.]*$")):
+    if not pat.match(arg_value):
+        raise argparse.ArgumentTypeError("Invalid model")
+    return arg_value
+
 class TulpArgs:
     _instance = None
+
 
     def __new__(cls):
         if cls._instance is None:
@@ -20,7 +27,7 @@ Intelligence world, backed by chatGPT.
             parser.add_argument('-w', type=str, help='Write the output (or the created program for execution) to the file. If the file exists, a backup will be created before overwriting it.')
 
 
-            parser.add_argument('--model', type=str, choices=['gpt-3.5-turbo', 'gpt-4'], help='Select the LLM model to use, currently gpt-3.5-turbo or gpt-4')
+            parser.add_argument('--model', type=model_type, help='Select the openai LLM model to use (default: gpt-3.5-turbo-16k)')
 
             parser.add_argument('--max-chars', type=int, help='Number of chars per message chunk per request')
 
